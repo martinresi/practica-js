240a1d929dabd4641121c0f9cc97d4b173d3f37b
martin <catalanoresimartin@gmail.com>
2023-07-12 12:31:33 -0300

    newCommitClase7

--- introduccion-a-js-master/tareas/clase-5/martin.js
+++ introduccion-a-js-master/tareas/clase-5/martin.js
@@ -1,12 +1,13 @@
-function crearIngresante(numero){
+function crearIngresante(numero) {
     const $div = document.createElement('div');
-    $div.className ="ingresantes";
-    const $label  = document.createElement("label");
-    $label.textContent ='Edad del integrante #: ' + (numero+ 1);
+    $div.className = "ingresantes";
+    const $label = document.createElement("label");
+    $label.textContent = 'Edad del integrante #: ' + (numero + 1);
     const $input = document.createElement("input");
 
     $input.type = "number";
-    
+    $input.setAttribute("class", "ingresantes-input");
+
     $div.appendChild($label);
     $div.appendChild($input);
 
@@ -16,107 +17,116 @@ function crearIngresante(numero){
 }
 
 
-document.querySelector('#boton-siguiente').onclick = function(event){ //boton para calcular las edades
+document.querySelector('#boton-siguiente').onclick = function (event) { //boton para calcular las edades
+    event.preventDefault();
+
     const $cantidadIngresantes = document.querySelector("#ingreso-integrantes");
     const cantIngresantes = Number($cantidadIngresantes.value);
 
-
     removerIngreso();
-
     ingresar(cantIngresantes);
-    
-    event.preventDefault();
+
+    // event.preventDefault();
+
 };
-document.querySelector('#calcular').onclick =function(event){
-    const numero = obtenerEdades();
-    document.querySelector('#edad-mayor').textContent = `la edad del mayor es ${obtenerMayor(numero)} `;
-    document.querySelector('#edad-menor').textContent =  `la edad del menor es ${obtenerMenor(numero)}`;
-    document.querySelector('#edad-promedio').textContent =  `la edad promedio' ${obtenerPromedio(numero)}`;
-    mostrarResultados();
+document.querySelector('#calcular').onclick = function (event) {
     event.preventDefault();
+    
+    // const numero = obtenerEdades();
+    const numero = obtenerEdad();
+    document.querySelector('#edad-mayor').textContent = `la edad del mayor es ${obtenerMayor(numero)} `;
+    document.querySelector('#edad-menor').textContent = `la edad del menor es ${obtenerMenor(numero)}`;
+    document.querySelector('#edad-promedio').textContent = `la edad promedio' ${obtenerPromedio(numero)}`;
+    mostrarResultados();
+    // event.preventDefault();
 };
 
-function ingresar(ingresantes){
-    if(ingresantes > 0){
+function ingresar(ingresantes) {
+    if (ingresantes > 0) {
         mostrarBotonCalcular();
-        
-    }  
-    else{
+
+    }
+    else {
         reset();
     }
     for (let i = 0; i < ingresantes; i++) {
         crearIngresante(i);// me crea un ingreso en cada posicion       
     }
-     
+
 }
 
 
-function obtenerMayor(numeros){
+function obtenerMayor(numeros) {
     let mayorNumero = numeros[0];
-    for (let i=0;i<numeros.length;i++){
-        if (numeros[i]>mayorNumero){
-            mayorNumero=numeros[i];
+    for (let i = 0; i < numeros.length; i++) {
+        if (numeros[i] > mayorNumero) {
+            mayorNumero = numeros[i];
         }
     }
-  
+
     return mayorNumero;
-  }
-  
-  function obtenerMenor(numeros){
+}
+
+function obtenerMenor(numeros) {
     let menorNumero = numeros[0];
-    for (let i=0;i<numeros.length;i++){
-        if(numeros[i]<menorNumero){ 
-            menorNumero=numeros[i];
+    for (let i = 0; i < numeros.length; i++) {
+        if (numeros[i] < menorNumero) {
+            menorNumero = numeros[i];
         }
     }
-  
+
     return menorNumero;
-  }
-  
-  function obtenerPromedio(numeros){
-    let acumulador=0;
-    for (let i=0;i<numeros.length;i++){
+}
+
+function obtenerPromedio(numeros) {
+    let acumulador = 0;
+    for (let i = 0; i < numeros.length; i++) {
         acumulador += numeros[i];
     }
-    const promedio = acumulador/numeros.length;
-  
-    return promedio;
-  }
+    const promedio = acumulador / numeros.length;
 
-function obtenerEdad(){
-    const $ingreso =document.querySelector('.ingresantes input');
+    return promedio;
+}
+
+function obtenerEdad() {
+    // const $ingreso = document.querySelector('.ingresantes input');
+    const $ingreso = document.getElementsByClassName('ingresantes-input');
+
     const edades = [];
-    for (let i = 0; i< $ingreso.length; i++) {
-        edades.push(Number($ingreso[i].value));    
+    for (let i = 0; i < $ingreso.length; i++) {
+        edades.push(Number($ingreso[i].value));
     }
     return edades;
 }
 
-
-function removerIngreso(){
-    const $ingresos =document.querySelector(".ingresantes");
-    for (let i = 0; i< $ingresos.length; i++) {
-        $ingresos[i].remove();
+function removerIngreso() {
+    /*
+    const $ingresos = document.querySelector(".ingresantes");
+    for (let i = 0; i < ingresos.length; i++) {
+        ingresos[i].remove();
     }
+    */
+    document.querySelector("#cantintegrantes").innerHTML = "";
+
 }
-function ocultarBotonCalcular(){
+function ocultarBotonCalcular() {
     document.querySelector("#calcular").className = 'oculto';
-  }
-  
-  
-  function mostrarBotonCalcular(){
+}
+
+
+function mostrarBotonCalcular() {
     document.querySelector("#calcular").className = '';
-  }
+}
 
-  function ocultarResultados(){
-    document.querySelector("#resultadosCalculos").className= 'oculto';
-  }
- function mostrarResultados(){
-    document.querySelector("#resultadosCalculos").className= '';
-  }
+function ocultarResultados() {
+    document.querySelector("#resultadosCalculos").className = 'oculto';
+}
+function mostrarResultados() {
+    document.querySelector("#resultadosCalculos").className = '';
+}
 
-  function reset(){
+function reset() {
     removerIngreso();
     ocultarBotonCalcular();
     ocultarResultados();
-  }
+}
